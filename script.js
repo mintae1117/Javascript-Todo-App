b8b4c6425bf2eecbcf3e5ea631397664db902050
mintae kim <peterzzo@naver.com>
2023-12-10 17:20:32 +0900

    first commin

---
+++ script.js
@@ -0,0 +1,32 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onloginSubmit(event){
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings();
+}
+
+function paintGreetings(){
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    //show loginform
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onloginSubmit);
+}
+else{
+    //show greetings
+    paintGreetings();
+}
