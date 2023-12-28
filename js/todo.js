55c79699e03037fd2a7b20a90fef2735fd8386cc
mintae kim <peterzzo@naver.com>
2023-12-28 15:42:25 +0900

    231228 add todo

---
+++ js/todo.js
@@ -0,0 +1,29 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
