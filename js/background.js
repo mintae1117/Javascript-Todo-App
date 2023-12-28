f1142c1faa696459149416047fdf4e7f6321ec8b
mintae kim <peterzzo@naver.com>
2023-12-28 14:40:34 +0900

    231228 background change

--- js/background.js
+++ js/background.js
@@ -2,8 +2,6 @@ const images = ["001.jpg","002.jpg","003.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const bgImage = document.querySelector("img#bg");
 
 bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
