4e3d88d5b5639a1e8547891d23fab0c1285c82c4
mintae kim <peterzzo@naver.com>
2023-12-28 14:26:41 +0900

    231228 add background

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["001.jpg","002.jpg","003.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
