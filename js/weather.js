d7adbf68985f6c9f6f719d4b9315d345b719cb42
mintae kim <peterzzo@naver.com>
2024-01-02 19:50:22 +0900

    240102 add weather api

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+
+const API_KEY = "efc2951a1074442c8b241a7e99f12b5d";
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     console.log("you live in", lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json()).then(data =>{
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        weather.innerText = `Today's weather is ${data.weather[0].main} and temperature is ${data.main.temp}.`
+        city.innerText = `You live in ${data.name}.`
+    });
 }
 function onGeoError(){
     alert("Can't find you... No weather for you.");
