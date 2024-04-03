ae91b1feafa41fe17b9ba2323d2fc4d0250934cf
mintaeKim <peterzzo@naver.com>
2024-04-04 03:53:10 +0900

    create snow divs with js from original html divs

--- js/onoffcheck.js
+++ js/onoffcheck.js
@@ -2,6 +2,13 @@ const form = document.querySelector(".wrapper");
 const showsnow = document.querySelector(".snowform");
 const myswitch = document.getElementById("switch");
 
+const getsnow = document.getElementById("snowbox");
+for(let i = 0; i < 200; i++){
+    const newdiv = document.createElement('div');
+    newdiv.className = "snow";
+    getsnow.appendChild(newdiv);
+};// create snow div
+
 function checked(){
     const checking = myswitch.checked;
     localStorage.setItem("snowoff", checking);
